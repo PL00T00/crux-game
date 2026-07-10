2e95e1cb21fbfa1b0afef4fb8a2868231c660565
Jame <Jamesferris447@gmail.com>
2026-07-10 19:29:31 +1000

    continyed player script

--- src/player/player.gd
+++ src/player/player.gd
@@ -4,12 +4,9 @@ extends CharacterBody3D
 @export var JUMP_VELOCITY = 5.0
 @export var GRAVITY = 10.0
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not is_on_floor():
+		velocity.y -= GRAVITY * delta
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		
