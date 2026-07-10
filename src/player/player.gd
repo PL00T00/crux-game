50a50afac8f8e3d0e06986334e5e58d619925420
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-07-11 02:02:26 +1200

    code

--- src/player/player.gd
+++ src/player/player.gd
@@ -6,6 +6,7 @@ extends CharacterBody3D
 var min_pitch: float = deg_to_rad(-80)
 var max_pitch: float = deg_to_rad(80)
 var mouse_sensitivity = 0.002
+var speed_timser = 1
 
 # Don't change
 var yaw: float = 0.0
@@ -27,14 +28,17 @@ func _physics_process(delta: float) -> void:
 
 	var input_dir := Input.get_vector("Left", "Right", "Forward", "Back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
+	
+	if Input.is_action_pressed('ui_shift'):
+		speed_timser = 2
+	
 	if direction != Vector3.ZERO:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * SPEED * speed_timser
+		velocity.z = direction.z * SPEED * speed_timser
 	else:
 		velocity.x = move_toward(velocity.x, 0.0, SPEED)
 		velocity.z = move_toward(velocity.z, 0.0, SPEED)
-
+		
 	move_and_slide()
 
 #See if mouse move and change where look
