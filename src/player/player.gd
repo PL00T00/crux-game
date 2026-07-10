7755563511dc3eba5e462797ad00f88beada5ee7
sebashtioon <sebastiansuciu607@gmail.com>
2026-07-10 19:31:42 +1000

    thing

--- src/player/player.gd
+++ src/player/player.gd
@@ -4,9 +4,21 @@ extends CharacterBody3D
 @export var JUMP_VELOCITY = 5.0
 @export var GRAVITY = 10.0
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= GRAVITY * delta
-	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		
+
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir := Input.get_vector("Left", "Right", "Forward", "Back")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if direction != Vector3.ZERO:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0.0, SPEED)
+		velocity.z = move_toward(velocity.z, 0.0, SPEED)
+
+	move_and_slide()
