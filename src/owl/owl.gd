f4a54c157d50c18196864b00dd2cf44e70bb76d1
Henry Wauzivuff <145900606+HEofOttoman@users.noreply.github.com>
2026-07-12 09:36:06 +1000

    what am i doing atp 💔

--- src/owl/owl.gd
+++ src/owl/owl.gd
@@ -1,1 +1,26 @@
 extends Node3D
+
+var interacting : bool = false
+var player_area_box_active : bool = false
+var mimic_raycast_box_active : bool = false
+
+@export var Contents_Node : Node3D
+@export var UI_Sprite_Node : Sprite3D
+#@export var SubViewport_Node : SubViewport
+
+#@export var ActionToPress : InputEventAction
+
+func _ready() -> void:
+	toggle_interacting(false)
+
+signal action_triggered
+
+func toggle_interacting(interacting_val : bool):
+	Contents_Node.visible = interacting_val
+	interacting = interacting_val
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if interacting:
+		if Input.is_action_just_pressed('Interact'):
+			action_triggered.emit()
+			Global.char_move = false
