71cb44d0baa01411545fadc2acfc367bdaec2941
sebashtioon <sebastiansuciu607@gmail.com>
2026-07-12 09:51:14 +1000

    playerbody

--- src/main/spawnpoints.gd
+++ src/main/spawnpoints.gd
@@ -1,19 +1,14 @@
 extends Node3D
 
-
-
 func _on_firstbridge_body_entered(body: Node3D) -> void:
-	print(body.name)
-	if body.name == "Player":
+	if body.is_in_group(&"player_body"):
 		Global.checkpoint = $firstbridge.global_position
 
 
 func _on_secondbridge_body_entered(body: Node3D) -> void:
-	print(body.name)
-	if body.name == "Player":
+	if body.is_in_group(&"player_body"):
 		Global.checkpoint = $secondbridge.global_position
 
 func _on_thirdbridge_body_entered(body: Node3D) -> void:
-	print(body.name)
-	if body.name == "Player":
+	if body.is_in_group(&"player_body"):
 		Global.checkpoint = $thirdbridge.global_position
