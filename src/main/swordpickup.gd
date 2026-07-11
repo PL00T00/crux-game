4a7770d5ce3d3f260b12d1d648c683bfbac74ec6
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-07-12 11:13:04 +1200

    sword

---
+++ src/main/swordpickup.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_g"):
+
+
+func _on_collliswordujw_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_collliswordujw_body_exited(body: Node3D) -> void:
+	pass # Replace with function body.
